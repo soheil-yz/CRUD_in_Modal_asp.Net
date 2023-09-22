70535c65fc97085c235b4b07a794550e7a7fd55d
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-09-22 13:33:56 +0330

    .

--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -3,14 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ModalCRUD.ViewModels;
 
 namespace ModalCRUD.Services
 {
     public interface IProductService
     {
-        bool CreateOrEditProduct(CreateOrEditProductViewModel createOrEdit);
 
-        CreateOrEditProductViewModel FillCreateOrEditProductViewModel(int productId);
     }
 }
