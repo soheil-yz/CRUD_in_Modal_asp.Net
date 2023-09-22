4d90856f6ee264e46c321081328767966213345f
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-09-22 12:54:30 +0330

    .

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using CRUD_in_Modal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_in_Modal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
